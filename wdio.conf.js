ff87c3a295edd75d6550f51b90ade3ca8da7c559
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-21 16:21:13 +0200

    update project for webdriverio V5

---
+++ wdio.conf.js
@@ -0,0 +1,55 @@
+exports.config = {
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
+    // on a remote machine).
+    runner: 'local',
+
+    //
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+
+    maxInstances: 10,
+
+    capabilities: [{
+
+        maxInstances: 5,
+        browserName: 'chrome'
+    }],
+
+    logLevel: 'info',
+    //
+    // If you only want to run your tests until a specific amount of tests have failed use
+    // bail (default is 0 - don't bail, run all tests).
+    bail: 0,
+
+    baseUrl: 'http://localhost',
+    //
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: 10000,
+    //
+    // Default timeout in milliseconds for request
+    // if Selenium Grid doesn't send response
+    connectionRetryTimeout: 90000,
+    //
+
+
+    framework: 'mocha',
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+
+    services: ['trueautomation'],
+    path: '/'
+}
